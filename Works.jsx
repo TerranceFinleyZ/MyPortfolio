25faf7c31e132b80ab29ebc2933aff59298149fe
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2023-10-07 16:35:00 -0700

    Add files via upload

---
+++ Works.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function Contact() {
+  return (
+    <div>
+      <div>
+        <p>Email: zealotschrist@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact;
